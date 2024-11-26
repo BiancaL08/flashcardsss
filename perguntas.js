010f05195f082bbfdf629dc0c4d480a22ebdddb4
BiancaL08 <00001114680941sp@al.educacao.sp.gov.br>
2024-11-26 15:44:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'cores',
+    'Qual é o nome da varinha de Harry Potter?',
+    'Varinha de sabugueiro.'
+)
+
+criaCartao(
+    'cores',
+    'Qual é o nome do animal de estimação de Hermione Granger?',
+    'Crookshanks.'
+)
+
+criaCartao(
+    'cores',
+    'Qual é o nome do protagonista de Harry Potter?',
+    'Harry Potter.'
+)
+
+criaCartao(
+    'cores',
+    'Qual é o nome do vilão principal da série?',
+    'Voldemort.'
+)
